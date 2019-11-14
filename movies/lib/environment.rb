72d4fcfaf799d87989f30f40d543266bef0a9789
Queen TIng <QueenTing@Briannas-MacBook-Air.local>
2019-11-14 14:51:24 -0500

    first commit

---
+++ movies/lib/environment.rb
@@ -0,0 +1,12 @@
+module BoxOffice
+  class Error < StandardError; end
+  # Your code goes here...
+end
+
+require_relative "movies/version"
+require_relative "movies/commands"
+require_relative "movies/movies"
+
+
+
+
